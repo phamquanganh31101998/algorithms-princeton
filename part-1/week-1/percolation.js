011ecfc472bfb5160b9dd2c3558948d7554ef90a
Quang <quanganhpham31101998@gmail.com>
2023-12-18 10:51:42 +0700

    Percolation: init

--- part-1/week-1/percolation.js
+++ part-1/week-1/percolation.js
@@ -1,62 +1,130 @@
-// Write a program to run simulation
-function percolation(n) {
-  /*
-    * Weighted quick-union algorithms
-    * */
+// Specification
+// https://coursera.cs.princeton.edu/algs4/assignments/percolation/specification.php
 
-  // init idList arr - contains their parent-child connection
-  // 2 nodes will be connected if they have the same root
-  const idList = []
+/*
+  * Weighted quick-union algorithms
+  * */
+class WeightedQuickUnion {
+  idList = [];
+  sizes = [];
 
-  // count the number of node in the tree rooted at i
-  const sizes = []
-
-  for (let i = 0; i < n; i++) {
-    idList.push(i)
-    sizes.push(1)
+  constructor(n) {
+    for (let i = 0; i < n; i++) {
+      this.idList.push(i)
+      this.sizes.push(1)
+    }
   }
 
-  function findRoot(x) {
-    while (x !== idList[x]) {
-      // path compression (but I'm not fully understand this)
-      idList[x] = idList[idList[x]]
-      x = idList[x]
+  findRoot(x) {
+    while (x !== this.idList[x]) {
+      this.idList[x] = this.idList[this.idList[x]]
+      x = this.idList[x]
     }
     return x
   }
 
-  // check if 2 nodes is connected
-  // by checking if they have the same root id
-  function isConnected(a, b) {
-    return findRoot(a) === findRoot(b)
+  isConnected(x, y) {
+    return this.findRoot(x) === this.findRoot(y)
   }
 
-  // connect 2 components at their node
-  // by connect root of smaller tree to the root of bigger tree (weighting)
-  function union(a, b) {
-    const rootOfA = findRoot(a)
-    const rootOfB = findRoot(b)
+  union(a, b) {
+    const rootOfA = this.findRoot(a)
+    const rootOfB = this.findRoot(b)
 
-    if (rootOfA === rootOfB) return
+    if (this.rootOfA === this.rootOfB) return
 
-    if (sizes[rootOfA] < sizes[rootOfB]) {
-      idList[rootOfA] = rootOfB
-      sizes[rootOfB] += sizes[rootOfA]
+    if (this.sizes[rootOfA] < this.sizes[rootOfB]) {
+      this.idList[rootOfA] = rootOfB
+      this.sizes[rootOfB] += this.sizes[rootOfA]
     } else {
-      idList[rootOfB] = rootOfA
-      sizes[rootOfA] += sizes[rootOfB]
+      this.idList[rootOfB] = rootOfA
+      this.sizes[rootOfA] += this.sizes[rootOfB]
+    }
+  }
+}
+
+// Write a program to run simulation
+class Percolation {
+  grid = [];
+  wqu = null;
+  vertex = 0;
+
+  // creates n-by-n grid, with all sites initially blocked
+  constructor(n) {
+    this.vertex = n
+    this.wqu = new WeightedQuickUnion(n)
+
+    for (let i = 0; i < n * n; i++) {
+      this.grid.push(false)
     }
   }
 
-  /*
-  * The actual simulation
-  * */
+  getPosition(row, col) {
+    return this.vertex * (row - 1) + (col - 1)
+  }
+
+  // opens the site (row, col) if it is not open already
+  open(row, col) {
+    if (!this.isOpen(row, col)) {
+      const position = this.getPosition(row, col)
+      this.grid[position] = true
+    }
+  }
+
+  // is the site (row, col) open?
+  isOpen(row, col) {
+    const position = this.getPosition(row, col)
+    return this.grid[position] === true
+  }
+
+  // is the site (row, col) full?
+  isFull(row, col) {
+
+  }
+
+  // returns the number of open sites
+  numberOfOpenSites() {
+    return this.grid.reduce((totalNum, currNode) => {
+      return !!currNode ? totalNum + 1 : totalNum
+    }, 0)
+  }
+
+  // does the system percolate?
+  isPercolates() {
+    // TODO: implement this
+    return false
+  }
+
+  info() {
+    console.log({
+      vertex: this.vertex,
+      grid: this.grid,
+      openSites: this.numberOfOpenSites()
+    })
+  }
+}
+
+// helpers
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function main() {
-  const avgProbability = 0;
-  // TODO: return the probability
-  percolation(30)
+  const n = 3
+  const p = new Percolation(n)
+
+  let time = 0
+
+  while (!p.isPercolates() && time < 5) {
+    const row = getRandomInt(1, n)
+    const col = getRandomInt(1, n)
+    p.open(row, col)
+    time++
+  }
+
+  p.info()
 }
 
 main()
